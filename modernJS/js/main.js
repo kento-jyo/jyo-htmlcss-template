a4b1f6ff3eaa3ed82612984c20705bf13389cbe7
kento betto <kento@jyo.jp>
2026-03-19 14:22:37 +0900

    基礎的なmodernJSのコードを追加

--- modernJS/js/main.js
+++ modernJS/js/main.js
@@ -1,28 +1,31 @@
+// main.js
+
+// const,letの違い
 const userName = "賢人";
 let age = 25;
 
-// テンプレートリテラルで表示
+
+// テンプレートリテラル
 console.log(`【初期状態】名前: ${userName}, 年齢: ${age}`);
 
 
-
+// 分割代入
 const user = {
     id: 1,
     name: "賢人",
     hobby: "プログラミング"
 };
 
-// 分割代入で hobby だけ抜き出す
-const { hobby } = user;
-console.log(`趣味は ${hobby} です✨`);
+const { id, name, hobby } = user;
+console.log(`私の社員idは${id}で、名前は${name}、趣味は ${hobby} です✨`);
 
-// スプレッド演算子で「新しい趣味」を追加した別オブジェクトを作る（memcpy + α）
+
+// スプレッド演算子
 const updatedUser = { ...user, hobby: "JavaScriptマスター", rank: "A" };
 console.log("更新後のデータ:", updatedUser);
 
 
-
-// C言語の関数宣言よりずっと楽ちん！
+// アロー関数
 const introduce = (name, targetHobby) => {
     return `${name}さんの新しい趣味は ${targetHobby} です！`;
 };
