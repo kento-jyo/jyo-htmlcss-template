5b98d8350ae2236ba9b800ff0444e8c7636663ae
kento betto <kento@jyo.jp>
2026-03-24 13:30:32 +0900

    ハンバーガーメニューのサンプルコードをdomフォルダに追加

--- dom/js/main.js
+++ dom/js/main.js
@@ -1,1 +1,11 @@
 // main.js
+const buttonContainer = document.querySelector('.p-button__container');
+const hamburger = document.querySelector('.p-hamburger');
+const hamburgerIcon = document.querySelector('.p-hamburger__icon');
+const xbuttonIcon = document.querySelector('.p-xbutton__icon');
+
+buttonContainer.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    hamburgerIcon.classList.toggle('active');
+    xbuttonIcon.classList.toggle('active');
+});
